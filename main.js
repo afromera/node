f753b91036cf94abee748f85886cddd699ea79d1
Alex Fernandes Romera <af_romera@hotmail.com>
2019-01-16 00:24:29 -0200

    Módulos Personalizados

--- main.js
+++ main.js
@@ -1,16 +1,11 @@
+const fatorial = require('./fatorial/start')
+
 console.log('n-fatorial')
 
 /*console.log(`Executando o script a partir do diretório ${process.cwd()}`)
 process.on('exit',() => {
     console.log('O processo está prestes a terminar')
 })*/
-
-const fatorial = (num) => {
-    if(num === 0)
-        return 1
-
-    return num * fatorial (num -1)
-}
 
 const num = parseInt(process.argv[2])
 console.log(`O fatorial de ${num} é igual a ${fatorial(num)}`)
